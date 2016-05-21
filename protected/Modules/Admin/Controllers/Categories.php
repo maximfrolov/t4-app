7cbd607636cd1837bb691601122d3c19d8d09ec5
Maxim Frolov <consumer2@mail.ru>
2016-05-21 17:18:51 +0500

    + добавление, редактирование и удаление категорий

--- protected/Modules/Admin/Controllers/Categories.php
+++ protected/Modules/Admin/Controllers/Categories.php
@@ -18,4 +18,56 @@ class Categories
         $this->data->cats = Category::findAllTree();
     }
 
+    /**
+     * @param null $id
+     */
+    public function actionEdit($id = null)
+    {
+        if (null !== $id) {
+            $this->data->cat = Category::findByPK($id);
+            if (empty($this->data->cat)) {
+                $this->redirect('/admin/categories');
+            }
+        } else {
+            $this->redirect('/admin/categories');
+        }
+    }
+
+    public function actionUpdate()
+    {
+        if (!empty($this->app->request->post->id)) {
+            $cat = Category::findByPK($this->app->request->post->id);
+            $cat->fill($this->app->request->post);
+            $cat->save();
+        }
+        $this->redirect('/admin/categories');
+    }
+
+    public function actionAdd()
+    {
+        $this->data->cats = Category::findAllTree();
+    }
+
+    public function actionSave($cat)
+    {
+        if (!empty($cat->title)) {
+            $catNew = new Category();
+            $catNew->fill($cat);
+            $catNew->save();
+        }
+        $this->redirect('/admin/categories');
+    }
+
+    /**
+     * @param int $id
+     */
+    public function actionDelete(int $id)
+    {
+        $cat = Category::findByPK($id);
+        if (!empty($cat)) {
+            $cat->delete();
+        }
+        $this->redirect('/admin/categories');
+    }
+
 }
